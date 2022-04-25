e2043e8074ff781e65012d412952ee081e3c1c8d
ericknathan <erick.capito@hotmail.com>
2022-04-25 11:30:04 -0300

    Recebimento de informações atualizado

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import { Ingest, Search } from 'sonic-channel';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { Ingest, Search } from "sonic-channel";
 
 const app = express();
 
@@ -8,54 +8,80 @@ app.use(cors());
 app.use(express.json());
 
 const sonicConfig = {
-  host: 'localhost',
+  host: "localhost",
   port: 1491,
-  auth: 'grati_search_microservice',
+  auth: "grati_search_microservice",
 };
 
 const sonicChannelIngest = new Ingest(sonicConfig);
 const sonicChannelSearch = new Search(sonicConfig);
 
 sonicChannelIngest.connect({
-  connected: () => console.log('[Sonic Ingest Channel] Connected!'),
-  error: (err) => console.log('[Sonic Ingest Channel] Error', err),
+  connected: () => console.log("[Sonic Ingest Channel] Connected!"),
+  error: (err) => console.log("[Sonic Ingest Channel] Error", err),
 });
 sonicChannelSearch.connect({
-  connected: () => console.log('[Sonic Search Channel] Connected!'),
-  error: (err) => console.log('[Sonic Search Channel] Error', err),
+  connected: () => console.log("[Sonic Search Channel] Connected!"),
+  error: (err) => console.log("[Sonic Search Channel] Error", err),
 });
 
-app.post('/user/:organization_id/:id', async (request: Request, response: Response) => {
-  const { name, username, responsibility, about, skills, graduations } = request.body;
-  const { organization_id, id } = request.params;
+app.post(
+  "/user/:organization_id/:id",
+  async (request: Request, response: Response) => {
+    const { name, username, responsibility, about, skills, graduations } =
+      request.body;
+    const { organization_id, id } = request.params;
 
-  await sonicChannelIngest.push('users', organization_id, `user:${id}`, `${name}, ${username}, ${responsibility}, ${about}, ${skills}, ${graduations}`, {
-    lang: 'por',
-  });
+    await sonicChannelIngest.push(
+      "users",
+      organization_id,
+      `user:${id}`,
+      `${name}, ${username}, ${responsibility}, ${about.replace('.', ' ')}, ${skills.replace('.', ' ')}, ${graduations.replace('.', ' ')}`,
+      {
+        lang: "por",
+      }
+    );
 
-  return response.status(201).send();
-});
+    return response.status(201).send();
+  }
+);
 
-app.get('/search/:organization_id', async (request: Request, response: Response) => {
-  const { q } = request.query;
-  const { organization_id } = request.params;
+app.get(
+  "/search/:organization_id",
+  async (request: Request, response: Response) => {
+    const { q } = request.query;
+    const { organization_id } = request.params;
 
-  const results = await sonicChannelSearch.query('users', organization_id, q, {
-    lang: 'por',
-  });
+    const results = await sonicChannelSearch.query(
+      "users",
+      organization_id,
+      q,
+      {
+        lang: "por",
+      }
+    );
 
-  return response.json(results);
-});
+    return response.json(results);
+  }
+);
 
-app.get('/suggest/:organization_id', async (request: Request, response: Response) => {
-  const { q } = request.query;
-  const { organization_id } = request.params;
+app.get(
+  "/suggest/:organization_id",
+  async (request: Request, response: Response) => {
+    const { q } = request.query;
+    const { organization_id } = request.params;
 
-  const results = await sonicChannelSearch.suggest('users', organization_id, q, {
-    limit: 5,
-  });
+    const results = await sonicChannelSearch.suggest(
+      "users",
+      organization_id,
+      q,
+      {
+        limit: 5,
+      }
+    );
 
-  return response.json(results);
-});
+    return response.json(results);
+  }
+);
 
 export { app };
